88fcd4741035a050fba9fcbcfcfade9f289c2f01
stellaxxy <stellaxxy0109@gmail.com>
2019-10-17 23:11:56 -0700

    signup endpoint in process

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 const PORT = process.env.PORT || 9000;
 const ENV = process.env.NODE_ENV || 'development';
 const mysql = require('mysql');
 const db = require('./db');
 const app = express();
 
-app.use(cors());
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
+
+app.post('/api/signup', (request, response) => {
+    try {
+        if(!request.body.username || !request.body.email || !request.body.password){
+            if(!request.body.username){
+                throw new Error('Please provide valid username.');
+            }
+            if(!request.body.email){
+                throw new Error('Please provide valid email.');
+            }
+            if(!request.body.password){
+                throw new Error('Please provide valid password');
+            }
+        } else {
+            let {username, email, password} = request.body;
+            const saltRounds = 10;
+            bcrypt.hash(password, saltRounds, async (err, hash) => {
+               if(err){
+                   throw new Error('Sorry an error has occurred. Please try again later.');
+               } else {
+                   password = hash;
+                   const query = "INSERT INTO `users` (`username`, `email`, `password`) VALUES (?, ?, ?)";
+                   const insertInfo = [username, email, password];
+                   const formattedQuery = mysql.format(query, insertInfo);
+                   const result = await db.query(formattedQuery);
+
+
+                   response.send({success: true});
+               }
+            });
+        }
+    } catch(err) {
+        handleError(response, err.message);
+    }
+});
 
 app.get('/api/newsfeed', (request, response) => {
     response.send({
